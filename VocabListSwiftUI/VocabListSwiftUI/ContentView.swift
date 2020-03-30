23cd2a5c90b0002367b1ad0e8e7f77a1f7295917
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2020-03-30 09:07:29 +0630

    create and build project

---
+++ VocabListSwiftUI/VocabListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VocabListSwiftUI
+//
+//  Created by Naw Su Su Nyein on 3/30/20.
+//  Copyright © 2020 Naw Su Su Nyein. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
